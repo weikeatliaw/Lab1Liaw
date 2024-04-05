480a9183eafc61ff2d049a37fb1224d4cbaa91b2
Liaw <wliaw@rockets.utoledo.edu>
2024-04-05 17:51:58 -0400

    first commit

---
+++ Lab1Liaw.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void) {
+
+	float width;
+	float length;
+	float area;
+	length = 4.5;
+	width = 3.35;
+	area = length * width;
+	cout << "Reactangle Area = " << area << "\n";
+		cout << "Who's on First? \n";
+
+	return 0;
+
+}
